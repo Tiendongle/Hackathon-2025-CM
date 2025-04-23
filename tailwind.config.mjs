c3451943cddd9bcd6d3cda430f2bfdab508b8d66
Tien Le <tien@tiendongle.com>
2025-04-22 22:25:43 -0700

    docs: Add Tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+// Example to modify Tailwind Config
+// https://github.com/apptainer/apptainer.org/blob/master/tailwind.config.mjs
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-}
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+};
