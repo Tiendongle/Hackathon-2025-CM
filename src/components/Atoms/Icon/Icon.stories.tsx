f2ca257a0922d06cca286f2527db77565c69c47e
Kutay <kutay.furtun@criticalmass.com>
2025-04-27 08:25:38 -0700

    implement feedback

--- src/components/Atoms/Icon/Icon.stories.tsx
+++ src/components/Atoms/Icon/Icon.stories.tsx
@@ -1,9 +1,16 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import Icon from "./Icon";
+import { iconMap, IconName } from "./iconMap";
 
 const meta: Meta<typeof Icon> = {
   component: Icon,
+  argTypes: {
+    iconName: {
+      control: { type: "select" },
+      options: Object.keys(iconMap),
+    },
+  },
 };
 
 export default meta;
@@ -14,4 +21,19 @@ export const Default: Story = {
   args: {
     iconName: "plus",
   },
+  render: (arg) => (
+    <div className="flex flex-col w-fit items-center">
+      <div className="flex flex-col  justify-center items-center py-5 w-52 aspect-square">
+        <Icon className="w-full h-full" {...arg} />
+      </div>
+      <div className="flex justify-center items-center">
+        {Object.keys(iconMap).map((type) => (
+          <span className="flex flex-col items-center px-2">
+            <Icon iconName={type as IconName} />
+            <p>{type}</p>
+          </span>
+        ))}
+      </div>
+    </div>
+  ),
 };
