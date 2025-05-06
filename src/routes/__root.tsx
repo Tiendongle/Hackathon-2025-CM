b8b4fb6388b4a65d01929f1365393b818e65efc4
Tiendongle <tien@tiendongle.com>
2025-05-05 23:11:21 -0700

    update max-width

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -100,7 +100,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <head>
         <HeadContent />
       </head>
-      <body className="max-w-[414px] mx-auto bg-neutral-100">
+      <body className="max-w-[550px] mx-auto bg-neutral-100">
         {headerDetails && <GlobalHeader headerDetails={headerDetails} />}
         {children}
         <footer className="sticky bottom-0 w-full px-2 bg-white text-black z-10">
